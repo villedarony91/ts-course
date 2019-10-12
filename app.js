1c6378f85e878dd1d793bb4ee22c8e2eeddb47bf
rl <villedarony91@gmail.com>
2019-10-12 13:54:48 -0600

    Exercise 1

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("It works!");
+// Exercise 1
+var double = function (value) { return value * 2; };
+console.log(double(10));
+// Exercise 2
+var greet = function (name) {
+    if (name === void 0) { name = "Max"; }
+    return console.log("Hello " + name);
+};
+greet();
+greet("Rony");
+// Exercise 3
+var numbers = [-3, 33, 38, 5];
+console.log(Math.min.apply(Math, numbers));
+// Exercise 4
+var newArray = [55, 20];
+newArray.push.apply(newArray, numbers);
+// Exercise 5
+var testResults = [3.89, 2.99, 1.38];
+var result1 = testResults[0], result2 = testResults[1], result3 = testResults[2];
+console.log(result1, result2, result3);
+// Exercise 6
+var scientist = { firstName: "Will", experience: 12 };
+var firstName = scientist.firstName, experience = scientist.experience;
+console.log(firstName, experience);
--- app.ts
+++ app.ts
@@ -1,1 +1,27 @@
-console.log("It works!");
+// Exercise 1
+const double =  value => value * 2
+console.log(double(10))
+
+// Exercise 2
+const greet =  (name = "Max") => console.log("Hello " + name)
+greet()
+greet("Rony")
+
+// Exercise 3
+const numbers = [-3, 33, 38, 5]
+console.log(Math.min(...numbers))
+
+// Exercise 4
+const newArray = [55, 20];
+newArray.push(...numbers)
+
+// Exercise 5
+const testResults = [3.89, 2.99, 1.38];
+const [result1, result2, result3] = testResults
+console.log(result1, result2, result3)
+
+// Exercise 6
+const scientist = {firstName: "Will", experience: 12};
+const { firstName , experience } = scientist
+console.log(firstName, experience)
+
